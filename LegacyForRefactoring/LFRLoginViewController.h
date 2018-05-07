dd547c475e874918d64248a91574cde172ff8e9d
mobile.admin <ios@tec-electronics.ru>
2018-05-07 14:22:21 +0200

    Initial commit

---
+++ LegacyForRefactoring/LFRLoginViewController.h
@@ -0,0 +1,13 @@
+//
+//  LFRLoginViewController.h
+//  LegacyForRefactoring
+//
+//  Created by Oleg Kokhtenko on 07/05/2018.
+//  Copyright © 2018 Kohtenko. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LFRLoginViewController : UIViewController
+
+@end
